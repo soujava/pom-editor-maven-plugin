52b9224b274e89dcdb42aef10b70d6a0721b57b2
Maximillian Arruda <dearrudam@gmail.com>
2023-03-18 19:08:19 -0300

    test[integration]: create an IT for GreetingMojo
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- src/test/java/io/github/arrudalabs/pomeditor/it/GreetingMojoIT.java
+++ src/test/java/io/github/arrudalabs/pomeditor/it/GreetingMojoIT.java
@@ -1,2 +1,41 @@
-package io.github.arrudalabs;public class GreetingMojoIT {
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.arrudalabs.pomeditor.it;
+
+import com.soebes.itf.jupiter.extension.MavenGoal;
+import com.soebes.itf.jupiter.extension.MavenJupiterExtension;
+import com.soebes.itf.jupiter.extension.MavenTest;
+import com.soebes.itf.jupiter.extension.SystemProperties;
+import com.soebes.itf.jupiter.extension.SystemProperty;
+import com.soebes.itf.jupiter.maven.MavenExecutionResult;
+
+import static com.soebes.itf.extension.assertj.MavenITAssertions.assertThat;
+
+@MavenJupiterExtension
+class GreetingMojoIT {
+
+    @MavenTest
+    @MavenGoal("${project.groupId}:${project.artifactId}:${project.version}:hello")
+    @SystemProperties(@SystemProperty(value = "username", content = "Max"))
+    void hello(MavenExecutionResult result) {
+        assertThat(result)
+                .isSuccessful()
+                .out()
+                .info().contains("Hi, Max!");
+    }
+
 }
