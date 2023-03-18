6605597cccd85000e0f10282d06e2af6084628a7
Maximillian Arruda <dearrudam@gmail.com>
2023-03-18 19:06:01 -0300

    chore: moved to correct package
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- src/test/java/io/github/arrudalabs/pomeditor/GreetingMojoTest.java
+++ src/test/java/io/github/arrudalabs/pomeditor/GreetingMojoTest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package io.github.arrudalabs;
+package io.github.arrudalabs.pomeditor;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
