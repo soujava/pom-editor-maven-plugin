a8946046523edf792dd061ec7d5ae4d97cf25315
Maximillian Arruda <dearrudam@gmail.com>
2023-03-18 17:54:32 -0300

    chore: added GreetingMojo with its Unit Test
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

---
+++ src/main/java/io/github/arrudalabs/GreetingMojo.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.arrudalabs;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+/**
+ * Says "Hi, ${username}!" to the output
+ */
+@Mojo(name = "hello")
+public class GreetingMojo extends AbstractMojo {
+
+    @Parameter(property = "username")
+    String username;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        getLog().info(String.format("Hi, %s!", username));
+
+    }
+}
