e0b6eb3b207ae8b1ca3dc8d192bad824f8d4a528
Maximillian Arruda <dearrudam@gmail.com>
2023-04-23 04:12:06 -0300

    test: create a class to provide invalid data
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

---
+++ src/test/java/br/org/soujava/pomeditor/InvalidGroupIdArtifactIdArgs.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2023  the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package br.org.soujava.pomeditor;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class InvalidGroupIdArtifactIdArgs implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
+        return Stream.of(
+                arguments(
+                        null, // groupId,
+                        null // artifactId,
+                ),
+                arguments(
+                        null, // groupId,
+                        "artifactId" // artifactId,
+                ),
+                arguments(
+                        "groupId", // groupId,
+                        null // artifactId,
+                ),
+                arguments(
+                        "", // groupId,
+                        "" // artifactId,
+                ),
+                arguments(
+                        "", // groupId,
+                        "artifactId" // artifactId,
+                ),
+                arguments(
+                        "groupId", // groupId,
+                        "" // artifactId,
+                )
+        );
+    }
+}
